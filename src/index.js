0161754f0637d4563f4e6be0b9445f0d70742f35
Alexandr <k.v.alexandr@gmail.com>
2020-09-29 02:55:02 +0400

    feat: task solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  return array && array.length ? Math.min(...array) : 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  return array && array.length ? Math.max(...array) : 0;
 }
 
-exports.avg = function avg (array) {
+exports.avg = function avg(array) {
+  if (array && array.length) {
+    const count = array.length;
+    const sum = array.reduce((a, e) => a + parseInt(e), 0);
+    return sum / count || 0;
+  }
   return 0;
 }
